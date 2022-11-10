2edfb77739cc9d3873805a7da207e979b82ae4e2
shashi1049 <shashi290396@gmail.com>
2022-11-10 23:59:01 +0530

    Buyerdaoimpl

---
+++ unit-5 project/src/com/project5/dao/Bbuyer.java
@@ -0,0 +1,9 @@
+package com.project5.dao;
+
+public interface Bbuyer {
+	
+	public String registerBuyer(String name,String email, int password);
+	// no need to make static and no need of retun type
+	// It will be implemented.
+
+}
